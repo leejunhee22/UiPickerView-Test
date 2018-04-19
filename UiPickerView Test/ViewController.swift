b015d8d4c82f46c3c4510ff8106b259fbfc4a431
D7703_17 <d7703_17@d7703-17ui-imac.local>
2018-04-19 11:52:45 +0900

    asdasdasd

--- UiPickerView Test/ViewController.swift
+++ UiPickerView Test/ViewController.swift
@@ -15,49 +15,30 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        myPickerView.delegate = self
-        myPickerView.dataSource = self
-    }
-    
-    // UIPickerViewDataSoure
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 2
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        if component == 0 {
-            return 10
-        } else {
-            return 100
-        }
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        if component == 0 {
-            return "First \(row)"
-        } else {
-            return "Second \(row)"
-        }
-    }
-    
-    // UIPickerViewDelegate
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-
-        if component == 0 {
-            col0Label.text = "First \(row)"
-        } else {
-            col1Label.text = "Second \(row)"
-        }
-    }
-    
-//    func pickerView(_ pickerView: UIPickerView, widthForComponent component: Int) -> CGFloat {
-//        return 15
-//    }
-    
-    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
-        return 50
-    }
-
+            myPickerView.delegate = self
+            myPickerView.dataSource = self
+      }
+      func numberOfComponents(in pickerView: UIPickerView) -> Int {
+            return 2
+      }
+      func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+            if component == 0 {
+                  return 10
+            } else {
+                  return 50
+            }
+      }
+      
+      func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+            if component == 0 {
+                  return "1st component \(row)"
+            } else {
+                  return "2nd component \(row)"
+            }
+      }
+      func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+            
+            col0Label.text = "comp = \(component+1) row = \(row)"
+      }
 }
 
